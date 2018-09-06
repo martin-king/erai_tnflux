9ca83584f0f822a7daa22dbda0c4347dd16b88c1
martin-king <martin-king@users.noreply.github.com>
2018-09-07 00:42:04 +0200

    Create cdo_mergeday.py

---
+++ cdo_mergeday.py
@@ -0,0 +1,8 @@
+import datetime
+print "module load cdo"
+
+years = range(1979,2016)
+for y in years:
+#   print "cdo -O merge *px*_"+str(y)+".nc *py*_"+str(y)+".nc *pz*_"+str(y)+".nc *div*_"+str(y)+".nc *psiaa*_"+str(y)+".nc erai_tnflux_day_"+str(y)
+#forget to use .nc above
+    print "mv erai_tnflux_day_"+str(y)+" erai_tnflux_day_"+str(y)+".nc"
